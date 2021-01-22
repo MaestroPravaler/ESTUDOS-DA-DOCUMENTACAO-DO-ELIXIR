0d7fc404c4dfae6bc9fee77f6f5b0b88b65d7896
RobsonMaestro <robson_maestro@alumni.usp.br>
2021-01-22 20:43:46 -0300

    1.5

---
+++ 01_Basic_Type/05_Strings.exs
@@ -0,0 +1,12 @@
+# Strings sempre delimitada por aspas duplas.
+
+IO.puts(is_binary("Uma String"))
+
+# Contar o número de bytes
+IO.puts(byte_size("Uma String"))
+
+# Contar o número de caracteres incluindo os espaços.
+IO.puts(String.length("Uma String"))
+
+# Caixa alta
+IO.puts(String.upcase("Uma String"))
