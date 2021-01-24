ee28317707f088bd38e5257e2ddc361672e24305
RobsonMaestro <robson_maestro@alumni.usp.br>
2021-01-24 10:49:28 -0300

    8.1

---
+++ 08_Recursion/01_Loops_Through_Recursion.exs
@@ -0,0 +1,14 @@
+# Utilizamos o recurso da recursão para escrever nossos loops
+
+defmodule Recursion do
+  def print_multiple_times(msg, n) when n <= 1 do
+    IO.puts msg
+  end
+
+  def print_multiple_times(msg, n) do
+    IO.puts msg
+    print_multiple_times(msg, n - 1)
+  end
+end
+
+Recursion.print_multiple_times("Hello!", 3)
