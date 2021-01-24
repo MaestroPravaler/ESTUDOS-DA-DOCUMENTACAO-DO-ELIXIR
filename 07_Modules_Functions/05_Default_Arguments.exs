91197683f35e734ee3881505f40c4d22ec1eb358
RobsonMaestro <robson_maestro@alumni.usp.br>
2021-01-24 10:33:03 -0300

    7.5

---
+++ 07_Modules_Functions/05_Default_Arguments.exs
@@ -0,0 +1,29 @@
+# Funções nomeadas no Elixir também suportam argumentos padrão:
+defmodule Concat do
+  def join(a, b, sep \\ " ") do
+    a <> sep <> b
+  end
+end
+
+IO.puts Concat.join("Hello", "world")      #=> Hello world
+IO.puts Concat.join("Hello", "world", "_") #=> Hello_world
+
+# Se uma função com valores padrão tem várias cláusulas, é necessário criar um
+# cabeçalho de função (sem um corpo real) para declarar os padrões:
+
+defmodule Concat do
+  # A function head declaring defaults
+  def join(a, b \\ nil, sep \\ " ")
+
+  def join(a, b, _sep) when is_nil(b) do
+    a
+  end
+
+  def join(a, b, sep) do
+    a <> sep <> b
+  end
+end
+
+IO.puts Concat.join("Hello", "world")      #=> Hello world
+IO.puts Concat.join("Hello", "world", "_") #=> Hello_world
+IO.puts Concat.join("Hello")               #=> Hello
